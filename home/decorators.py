6cea30b12a60b1f9d19ba3f07cac469f3088c264
vishalmighty <vishal@Vishal-Intern.local>
2023-02-17 13:56:41 +0530

    Removed the code(to check and redirect to admin home page if user is already logedin) from login_page function in views.py and added the same code as decorators in decorators.py file

--- home/decorators.py
+++ home/decorators.py
@@ -1,3 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+# its a wrapper fucntion(which takes function as parameter and do some checks before calling the actual function
+def unauthenticateduser(view_func):
+    def wrapper_func(request,*args,**kwargs):
+        if(request.user.is_authenticated):
+            return redirect('admin_home')
+        else:
+            return view_func(request, * args, **kwargs)
+    
+    return wrapper_func
--- home/views.py
+++ home/views.py
@@ -1,41 +1,52 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticateduser
 # Create your views here.
 
+
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
 
 def rolls(request):
-    return render(request,'rolls.html')
+    return render(request, 'rolls.html')
 
 # Supplier
+
+
 def supplier_home(request):
-    return render(request,'supplier_home.html')
+    return render(request, 'supplier_home.html')
 
 # Manufacturer
-def manufacturer_home(request):
-    return render(request,'manufacturer_home.html')
 
-# Retailer 
+
+def manufacturer_home(request):
+    return render(request, 'manufacturer_home.html')
+
+# Retailer
+
+
 def retailer_home(request):
-    return render(request,'retailer_home.html')
+    return render(request, 'retailer_home.html')
 
 # Admin items
-@login_required(login_url= 'admin_login')
+
+
+@login_required(login_url='admin_login')
 def admin_home(request):
-    return render(request,'admin.html')
+    return render(request, 'admin.html')
+
 
 def admin_signup(request):
-    if(request.user.is_authenticated):
+    if (request.user.is_authenticated):
         return redirect('admin_home')
     else:
-        form = CreateUserForm()     
+        form = CreateUserForm()
         print(request.POST)
-        if request.method=='POST':
+        if request.method == 'POST':
             form = CreateUserForm(request.POST)
             if form.is_valid():
                 form.save()
@@ -45,33 +56,32 @@ def admin_signup(request):
                 print("ERROR not valid form")
         else:
             print("ERROR")
-        context = {'form' : form }
-        return render(request,'admin_signup',context)
+        context = {'form': form}
+        return render(request, 'admin_signup', context)
 
+
+@unauthenticateduser
 def admin_login(request):
-    if(request.user.is_authenticated):
-        return redirect('admin_home')
-    else:
-        if request.method == 'POST':
-            email = request.POST.get('email')
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            # print(email)
-            # print(password)
-            user = authenticate(request, username=username,password=password)
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # print(email)
+        # print(password)
+        user = authenticate(request, username=username, password=password)
 
-            if user is not None:
-                login(request,user)
-                # print(user)
-                return redirect('admin_home')
-            else:
-                # print(user)
-                print("error in user match")
+        if user is not None:
+            login(request, user)
+            # print(user)
+            return redirect('admin_home')
         else:
-            print("ERROR in post method")
+            # print(user)
+            print("error in user match")
+    else:
+        print("ERROR in post method")
 
-        context = {}
-        return render(request,'admin_login.html',context)
+    context = {}
+    return render(request, 'admin_login.html',context)
 
 def admin_logout(request):
     logout(request)
