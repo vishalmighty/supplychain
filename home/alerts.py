508a196d35679bd8518a3dd28fb65a272c8222bf
bvishal47 <109956444+bvishal47@users.noreply.github.com>
2023-04-04 01:08:56 +0530

    Added alert alert feature

--- home/alerts.py
+++ home/alerts.py
@@ -1,6 +1,7 @@
 import time
 from django.utils import timezone
 from home.models import ManufacturerProduct, ProductAlert
+from django.core.mail import send_mail
 # from django_background_tasks import background
 
 # @background(schedule=60*30)
@@ -8,15 +9,19 @@ def check_inventory():
     while True:
         products = ManufacturerProduct.objects.all()
         for product in products:
+            recipient_list = []
             try:
                 alert = ProductAlert.objects.get(product=product)
                 if product.quantity < alert.alert_count:
-                    print(f"ALERT: {product.name} has quantity less than alert count.")
-                    # You can also create a log entry, send an email or notification, etc.
-                else:
-                    print("HII alert")
+                    subject = 'Alert'
+                    message = f"ALERT: {product.name} has quantity({product.quantity}) less than alert count({alert.alert_count})."
+                    from_email = 'vishal@example.com'
+                    recipient_list.append(product.user.email)
+                    print(f"ALERT: {product.name} has quantity({product.quantity}) less than alert count({alert.alert_count}).")
+                    # print(product.user.email)
+                    send_mail(subject, message, from_email, recipient_list)
             except:
                 pass
-        time.sleep(6)
+        time.sleep(60) #60 sec for testing purpose
 
     
--- scm/settings.py
+++ scm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from scm.env import email_user_name,email_user_password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,3 +156,11 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "home.User" 
+
+# SMTP email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = email_user_name
+EMAIL_HOST_PASSWORD = email_user_password
